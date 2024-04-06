e7af24fc8c3314f42f23736df93960230f53a86a
shanditom <shanditom@gmail.com>
2024-04-06 16:32:03 +0330

    1- sed data from render to template example name,email, ... 2- make base.html for using extends template tag

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request,'website/rezumeh.html')
+    contex={'name':'پیام عمرانی','job':'برنامه نویس','birthday':'۱۳۶۱','location':'تهران','email':'shanditom@gmail.com','mobile':'۰۹۱۲۸۳۶۹۸۶۶'}
+    return render(request,'website/rezumeh.html',contex)
